03c091e6c875a4b6461c42a733e786fed9ab41b4
frankharerimana <fharerimana@objectivity.co.uk>
2023-12-26 09:54:46 +0200

    remove unnecessary files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	plugins: [require("daisyui")],
 	content: [
 		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 		"./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
 	],
-	theme: {
-		screens: {},
-		extend: {
-			colors: {},
-		},
+	daisyui: {
+		themes: ['nord'],
+		darkTheme: "dark",
+		base: true,
+		styled: true,
+		utils: true,
+		prefix: "",
+		logs: true,
+		themeRoot: ":root",
 	},
 };
